4828dc77fbf51c903f838f85aaec3017c352cfb1
Anthony-a-l-s <alima.als37@gmail.com>
2022-06-28 21:30:05 -0300

    init tela home

--- Console.log-Front/src/Screens/Home/Home.tsx
+++ Console.log-Front/src/Screens/Home/Home.tsx
@@ -1,21 +1,71 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable global-require */
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
+import React from 'react';
+import NavBar from '../../Components/NavBar';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
   },
+  container2: {
+     flexDirection: 'row',
+     alignItems: 'center',
+     
+  },
+  text1: {
+    margin: 30,
+    fontSize: 19,
+  },
+  text2: {
+    marginBottom: 10,
+    fontSize: 18,
+    marginLeft: 10,
+  }
 });
+
+const discliplinas=[
+  {
+     nome: 'Estrutura de dados II',
+  },
+  {
+    nome: 'Teoria dos grafos',
+  },
+  {
+    nome: 'Organização de computadores'
+  },
+  {
+    nome: 'Sistemas Operacionais'
+  }
+  ]
 
 const Home: React.FC = ({ route, navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      
+      <Text style={styles.text1}>Selecione uma disciplina para comear:</Text>
+        <FlatList
+          data={discliplinas}
+          renderItem={({item})=>(
+            <TouchableOpacity  style={{margin: 5}}>
+              <View style={styles.container2}>
+                {/*<Image
+                style={{width: 50, height: 50}}
+                source={require('../../Assets/ed.png')}
+                 />*/}
+                <Text style={styles.text2}>{item.nome}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+  
+
     </View>
   );
 };
+
+
 
 export default Home;
