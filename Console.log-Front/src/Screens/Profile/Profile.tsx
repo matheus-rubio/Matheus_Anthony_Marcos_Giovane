0b3fd2c607ec4dc14f9a06f84a13f9035a5919b6
Anthony-a-l-s <alima.als37@gmail.com>
2022-06-29 23:28:57 -0300

    update tela profile

--- Console.log-Front/src/Screens/Profile/Profile.tsx
+++ Console.log-Front/src/Screens/Profile/Profile.tsx
@@ -1,6 +1,49 @@
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView, FlatList } from "react-native";
 import React, { useContext } from "react";
 import AuthUserContext from "../../Contexts/AuthUserContext/context";
+
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Profile: React.FC = ({ route, navigation }) => {
+  const { user } = useContext(AuthUserContext);
+  const image =
+    user?.type === "S"
+      ? require("../../Assets/aluno.png")
+      : require("../../Assets/professor.jpg");
+
+  const medalhas =['Top 1 - Estrutura de daddos', 'Top 1 - Teoria dos Grafos ', 'Top 2 - Algoritmos' ]
+
+  return (
+   
+     <View style={styles.container}>
+      <View style={styles.containerHeader}></View>
+      <View style={styles.containerImage}>
+        <Image style={styles.image} source={image} />
+        <Text style={styles.textoNome}>{user?.nm_user}</Text>
+        {user?.type === "T" && <Text style={styles.textoTipo}>Professor</Text>}
+        {user?.type === "S" && <Text style={styles.textoTipo}>Aluno</Text>}
+        <View style={styles.containerInformation}>
+          <View style={styles.containerInformationInside}>
+            <Text style={styles.textoMdelha}>Medalhas</Text>
+            <Text style={styles.textoQuiz}>Quizes</Text>
+          </View>
+          <ScrollView style={styles.containerMedalhas}>
+            {/*<FlatList
+             data ={medalhas}  
+                renderItem= {({item})=>(
+                  <View style={styles.conteinerContextMedalhas}>
+                  <Text style={styles.textoTipo}>{item}</Text>
+                  </View>
+                )}  
+                />*/}
+          </ScrollView>
+        </View>
+      </View>
+     </View>
+  
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -42,6 +85,25 @@ const styles = StyleSheet.create({
     width: "99%",
     height: 47,
   },
+  containerMedalhas: {
+    marginTop: 360,
+   backgroundColor: "#80BBE6",
+   flex: 1,
+   height: '580%',
+   width: '100%',
+   borderRadius: 30,
+   position: 'absolute',
+  },
+  conteinerContextMedalhas: {
+      marginLeft: 20,
+       backgroundColor: "#EEDDAA",
+       width: '90%',
+       height: '100%',
+       justifyContent: 'center',
+       alignItems: 'center',
+       marginTop: 20,
+       borderRadius: 30,
+  },
   image: {
     width: 240,
     height: 240,
@@ -64,36 +126,6 @@ const styles = StyleSheet.create({
   textoQuiz: {
     color: "#2F72BC",
   },
-  containerMedalhas: {
-    backgroundColor: "2F72BC",
-  },
 });
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Profile: React.FC = ({ route, navigation }) => {
-  const { user } = useContext(AuthUserContext);
-  const image =
-    user?.type === "S"
-      ? require("../../Assets/aluno.png")
-      : require("../../Assets/professor.jpg");
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.containerHeader}></View>
-      <View style={styles.containerImage}>
-        <Image style={styles.image} source={image} />
-        <Text style={styles.textoNome}>{user?.nm_user}</Text>
-        {user?.type === "T" && <Text style={styles.textoTipo}>Professor</Text>}
-        {user?.type === "S" && <Text style={styles.textoTipo}>Aluno</Text>}
-        <View style={styles.containerInformation}>
-          <View style={styles.containerInformationInside}>
-            <Text style={styles.textoMdelha}>Medalhas</Text>
-            <Text style={styles.textoQuiz}>Quizes</Text>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
-};
 
 export default Profile;
